9267142bc36f3a5c331fb9bb704c6839aa481e56
GuenterQuast <guenter.quast@online.de>
2018-07-22 14:31:49 +0200

    added differential mode for MCP3008

--- phypidaq/MCP3008Config.py
+++ phypidaq/MCP3008Config.py
@@ -14,15 +14,21 @@ class MCP3008Config(object):
   def __init__(self, confdict = None):
     if confdict==None: confdict={}
 
-    if 'NChannels' in confdict:
-      self.NChannels = confdict['NChannels']
+    if 'ADCChannels' in confdict:
+      self.ADCChannels = confdict['ADCChannels']
     else:
-      self.NChannels = 1
+      self.ADCChannels = [1]
+    self.NChannels = len(self.ADCChannels)
+
+    if 'DifModeChan' in confdict:
+       self.DifModeChan = confdict['DifModeChan']
+    else:
+      self.DifModeChan = [false] * self.NChannels
 
     if 'VRef' in confdict:
       self.VRef = confdict['VRef']
     else:
-      self.VRef = 3.3
+      self.VRef = 5.0
 
     if 'NBits' in confdict:
       NBits = confdict['NBits']
@@ -37,21 +43,38 @@ class MCP3008Config(object):
     SPI_PORT   = 0
     SPI_DEVICE = 0
     try:
-      self.MCP = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT,
-                           SPI_DEVICE, max_speed_hz=15600000) )
-                                                      # run bus at 15.6 MHz, default is 0.488MHz
+      self.MCP = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE) )
     except:
       print("MCP3008Config: Error initialising device - exit")
       sys.exit(1)
 
 # provide configuration parameters
-    self.ChanNams = [ str(i) for i in range(self.NChannels) ]
-    self.ChanLims = [ [0., self.VRef] for i in range(self.NChannels) ]
+    self.ChanNams = []
+    self.ChanLims = []
+    for i, c  in enumerate(self.ADCChannels):
+      isdifferential = self.DifModeChan[i]
+      if isdifferential:
+        if c%2:
+          d= str( c) + '-' + str(c-1) 
+        else:
+          d= str( c) + '-' + str(c+1) 
+      else:
+        d = str(c)
+      self.ChanNams.append(d) 
+      self.ChanLims.append(  [- isdifferential * self.VRef , self.VRef])
       
   def acquireData(self, buf): 
     # read data from ADC
-    for i in range(self.NChannels):
-      buf[i] = self.MCP.read_adc(i) * self.Vfac
+    for i, c  in enumerate(self.ADCChannels):
+      if self.DifModeChan[i]:
+        # need two readings or pair of channels, e.g.:   v = adc(0-1)  - adc(1-0)
+        cb=c - c%2    # get lowest channel of pair
+        v = (self.MCP.read_adc_difference(cb)\
+                      - self.MCP.read_adc_difference(cb+1)) * self.Vfac
+        if c%2:  v *= -1  # correct sign if odd channel number given    
+        buf[i] = v 
+      else:
+        buf[i] = self.MCP.read_adc(c) * self.Vfac
 
   def closeDevice(self):
    # nothing to do here
