11d9f0f173c7d9a84bff45c7046e857dc41fc933
Günter Quast <Guenter.Quast@online.de>
2018-10-13 11:53:44 +0200

    added script install_user.sh to set up files relevant to user

---
+++ install_user.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# script to initially copy files to user direcotry ~/PhyPi/
+#
+
+USERDIR="testPhyPi"  # user directory
+
+
+# -----------------------------------------
+
+DIR=$HOME/$USERDIR
+echo "copying files to "$DIR
+
+mkdir -p $DIR
+
+if [ -d $DIR ]; then
+  # enter here, if direcotry exists
+    # copy documentation
+  mkdir -p $DIR/doc
+  cp -auv doc/*.pdf $DIR/doc/
+  cp -auv README_de.pdf $DIR
+    #copy config examples
+  cp -auv config/ $DIR
+  cp -auv default.daq $DIR
+  cp -auv PhyPiConf.daq $DIR
+    # copy examples
+  cp -auv examples/ $DIR
+fi
