e9f0433a1e1b2224ca12bba5aa0672e4aaef8f47
Günter Quast <Guenter.Quast@online.de>
2018-10-31 20:34:32 +0100

    nicer text format

--- phypidaq/DataGraphs.py
+++ phypidaq/DataGraphs.py
@@ -220,7 +220,7 @@ class DataGraphs(object):
     # update text display
           endl = ''
           if i%2: endl = '\n'
-          txt += '  %s:   %.3g (%s)'% (self.ChanNams[i], self.Vhist[i,k], self.ChanUnits[i]) + endl 
+          txt += '  %s:   %.3g %s'% (self.ChanNams[i], self.Vhist[i,k], self.ChanUnits[i]) + endl 
     # update bar chart
           self.bgraphs[i].set_height(dat[i])
           self.animtxt.set_text(txt)
