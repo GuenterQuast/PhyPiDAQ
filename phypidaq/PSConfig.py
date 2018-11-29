d4243cc8ce38daf75c0898f0bd06f88b685cfb2d
Günter Quast <Guenter.Quast@online.de>
2018-11-29 18:58:22 +0100

    mean or rms averaging selectable in PSConfig.py

--- phypidaq/PSConfig.py
+++ phypidaq/PSConfig.py
@@ -31,12 +31,18 @@ class PSConfig(object):
     for i in range(self.NChannels):
      self.ChanLims.append( (-CRanges[i]-COffsets[i], 
                         CRanges[i]-COffsets[i]) )
-    
+# RMS or mean of samples ?
+    self.AvModes = self.NChannels * [0]
+    if self.confdict['ChanAverages']:
+      ChanAvs = self.confdict['ChanAverages']
+      if type(ChanAvs) is not type([]): ChanAvs = [ChanAvs]      
+      for i,m in enumerate(ChanAvs):
+        if m == 'rms': self.AvModes[i] = 1
+      print('Channel %i: averaging mode= %s'%(i,m))
+         
 # data buffer for PicoScope driver
     self.NSamples = self.PS.NSamples
     self.buf = np.zeros( (self.PS.NChannels, self.PS.NSamples) )
-
-
 
     
   def acquireData(self, sig): 
@@ -44,8 +50,10 @@ class PSConfig(object):
 
     self.PS.acquireData(self.buf) # read data from PicoScope
     for i, b in enumerate(self.buf): # process data 
-      #sig[i] = np.sqrt (np.inner(b, b) / self.NSamples)    # eff. Voltage
-      sig[i] = b.sum() / self.NSamples          # average
+      if self.AvModes[i]:
+        sig[i] = np.sqrt (np.inner(b, b) / self.NSamples)    # eff. Voltage
+      else:
+        sig[i] = b.sum() / self.NSamples          # average
 
   def closeDevice(self):
     self.PS.closeDevice()
