22e7c88af3449e31f53666f9265a6e4bd3a5db1c
Günter Quast <Guenter.Quast@online.de>
2019-01-18 23:53:46 +0100

    added installation script install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# script to install libraries PhyPiDAQ depends on
+#
+# -------------------------------------------------
+
+
+sudo apt-get install python3-scipy
+sudo apt-get install python3-matplotlib
+sudo apt-get install python3-pyqt5
+sudo apt-get install libatlas-base-dev # needed to build nupmy
+
+sudo pip3 install installlibs/whl/*.whl
+sudo dpkg -i installlibs/picoscopelibs/*.deb
+
+sudo usermod -a -G tty pi # grant acces to USB for user pi
+
