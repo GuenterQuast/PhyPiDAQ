c7745175210285e657e3c30b97ee7e866fb17d84
GuenterQuast <guenter.quast@online.de>
2019-04-14 10:38:24 +0200

    added GPIO.PWM to Frequency generator (for fast signals)

--- examples/FreqGen.py
+++ examples/FreqGen.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 """Frequency generator 
+    uses pulse width modulation for signal periods < 0.013 s
+    optional Arguments:
+      $1: GPIO pin
+      $2: signal period
+      $3: duty cycle of signal 
 """
+
 from __future__ import print_function, division, unicode_literals
 from __future__ import absolute_import
 
@@ -9,29 +15,52 @@ import RPi.GPIO as gpio
 gpio.setmode(gpio.BCM)
 import threading
 
-dt=0.02
-tflash=0.005
-GPIOpin = 19
+# initialize defaults
+GPIOpin = 19     # GPIO pin for output signal
+dt=0.02          # signal Period
+dc= 0.5          # signal duty cycle 
 
-gpio.setup(GPIOpin, gpio.OUT)
+dtslow=0.013    # "slow signal"
 
-def pulse(pin, dt):
+if len(sys.argv) > 1:
+  GPIOpin = int(sys.argv[1])
+if len(sys.argv) > 2:
+  dt = float(sys.argv[2])
+if len(sys.argv) > 3:
+  dt = float(sys.argv[3])
+
+def pulse(pin, ton):
   gpio.output(pin, 1)
-  time.sleep(dt)
+  time.sleep(ton)
   gpio.output(pin, 0)
 
-print('flashing GPIO pin %i with dt= %.3gs' %(GPIOpin, dt) )
+def pwmpulse(pin, dt, dc=0.5):
+  pwm = gpio.PWM(pin, 1./dt)
+  pwm.start(dc)     # start with duty cycle dc
+
+
+print('flashing GPIO pin %i with dt= %.3gs and duty cycle %.3g'%(GPIOpin, dt, dc) )
+if dt<dtslow: print('         using gpio.PWM()')
 
 try:
-  dtcum = 0.
-  T0 = time.time()
-  while True:
-    flashThread=threading.Thread(target=pulse, args=(GPIOpin, tflash,))
-    flashThread.start()
-    dtcor = dt - time.time() + T0  + dtcum
-    if dtcor > 0.: time.sleep(dtcor)
-    dtcum += dt
-                     
+  gpio.setup(GPIOpin, gpio.OUT)  # initialize GPIO pin for output
+  if dt >= dtslow: # use time.sleep for slow signals
+    ton = dt * dc  # active time of signal 
+    dtcum = 0.
+    T0 = time.time()
+    while True:
+      flashThread=threading.Thread(target=pulse, args=(GPIOpin, ton,))
+      flashThread.start()
+      dtcor = dt - time.time() + T0  + dtcum
+      if dtcor > 0.: time.sleep(dtcor)
+      dtcum += dt
+
+  else:  # use PWM for fast signals
+    pwm = gpio.PWM(GPIOpin, 1./dt)
+    pwm.start(dc)     # start with duty cycle dc
+    while True:
+      time.sleep(1.)
+
 except  KeyboardInterrupt:
   print ("keyboard interrupt - ending")
 
