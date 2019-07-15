46feebfe434dd24e37e413a0f4bdeec5f3bef0ba
JanKue <jan.kueblbeck@student.kit.edu>
2019-07-15 20:20:46 +0200

    model mockup facade to view

--- 1-FREEJDAQ/src/main/java/edu/kit/freejdaq/mockup/ModelMockup.java
+++ 1-FREEJDAQ/src/main/java/edu/kit/freejdaq/mockup/ModelMockup.java
@@ -1,8 +1,13 @@
 package edu.kit.freejdaq.mockup;
 
+import java.util.HashMap;
+
+import edu.kit.freejdaq.model.BuildingBlock;
+
 public class ModelMockup implements ControllerModelInterface {
 	
 	// IModelInformation
+	
 	public boolean addBuildingBlock(String id) {
 		System.out.println("added block; id: " + id);
 		return true;
@@ -24,6 +29,7 @@ public class ModelMockup implements ControllerModelInterface {
 	}
 	
 	// IMeasurementRun
+	
 	public boolean start(String[] sensorIds, IMStreamListener[] dataListeners) {
 		System.out.println("started");
 		return true;
@@ -38,6 +44,37 @@ public class ModelMockup implements ControllerModelInterface {
 	}
 	public boolean reset(){
 		System.out.println("resetted");
+		return true;
+	}
+	
+	// ViewDirectoryInterface
+	
+	public BuildingBlock getInitBuildingBlock(String id) {
+		System.out.println("getting init block with id " + id);
+		return null;
+	}
+	
+	public BuildingBlock getConfigBuildingBlock(int id) {
+		System.out.println("getting config block with id " + id);
+		return null;
+	}
+	
+	public HashMap<String, BuildingBlock> getFullInitBlock() {
+		System.out.println("getting all init blocks");
+		return null;
+	}
+	
+	public HashMap<Integer, BuildingBlock> getFullConfigBlock() {
+		System.out.println("getting all config blocks");
+		return null;
+	}
+	
+	public boolean getConnection(int chId1, int chId2) {
+		System.out.println("getting connection");
+		return true;
+	}
+	
+	public boolean checkForUpdate() {
 		return true;
 	}
 
