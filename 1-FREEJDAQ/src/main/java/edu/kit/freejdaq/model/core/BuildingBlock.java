efd7c0e9bb19272b0fdd7b5a9683df6aad2ab1ba
DavidGawron <david.gawron91@web.de>
2019-07-17 10:55:53 +0200

    attribute/ methoden buildingBlock / BBDirectory

--- 1-FREEJDAQ/src/main/java/edu/kit/freejdaq/model/core/BuildingBlock.java
+++ 1-FREEJDAQ/src/main/java/edu/kit/freejdaq/model/core/BuildingBlock.java
@@ -1,7 +1,9 @@
 package edu.kit.freejdaq.model.core;
 
 
+import edu.kit.freejdaq.GUI.Configuration.BuildingBlockView;
 import edu.kit.freejdaq.model.*;
+import java.util.ArrayList;
 import edu.kit.freejdaq.model.buildingBlockBuilder.*;
 import edu.kit.freejdaq.model.channelLogic.*;
 import edu.kit.freejdaq.model.core.*;
@@ -10,6 +12,80 @@ import edu.kit.freejdaq.model.representationLogic.*;
 import edu.kit.freejdaq.model.sensorLogic.*;
 import edu.kit.freejdaq.model.transformationLogic.*;
 
-public class BuildingBlock {
+abstract public class BuildingBlock {
+	private ArrayList<InChannel> inChannels;
+	private ArrayList<OutChannel> outChannels;
+	private long configId;
+	private String initId;
+	private String name;
+	private String userInfo;
+	
+	public BuildingBlock(String initId) {
+		this.inChannels =  new ArrayList<InChannel>();
+		this.outChannels =  new ArrayList<OutChannel>();
+		this.setInitId(initId);
+		
+		
+	}
+
+	
+	public void addInChannel(String id, String name) {
+		
+	}
+	public void addOutChannel(String id, String name) {
+		
+	}
+	public void removeInChannel(String id, String name) {
+		
+	}
+	public void removeOutChannel(String id, String name) {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getUserInfo() {
+		return userInfo;
+	}
+
+	public void setUserInfo(String userInfo) {
+		this.userInfo = userInfo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getInitId() {
+		return initId;
+	}
+
+	public void setInitId(String initId) {
+		this.initId = initId;
+	}
+
+	public long getConfigId() {
+		return configId;
+	}
+
+	public void setConfigId(long configId) {
+		this.configId = configId;
+	}
+	
 
 }
--- 1-FREEJDAQ/src/main/java/edu/kit/freejdaq/model/core/BuildingBlockDirectory.java
+++ 1-FREEJDAQ/src/main/java/edu/kit/freejdaq/model/core/BuildingBlockDirectory.java
@@ -2,6 +2,7 @@ package edu.kit.freejdaq.model.core;
 
 
 import edu.kit.freejdaq.model.*;
+import java.util.HashMap;
 import edu.kit.freejdaq.model.buildingBlockBuilder.*;
 import edu.kit.freejdaq.model.channelLogic.*;
 import edu.kit.freejdaq.model.core.*;
@@ -15,6 +16,67 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class BuildingBlockDirectory {
+	
+	
+	private HashMap initHashMap;
+	private HashMap configHashMap;
 	private static final Logger logger = LogManager.getLogger(BuildingBlockDirectory.class);
+	
+	
+	public BuildingBlockDirectory() {
+		
+		HashMap<String, BuildingBlock> initHashMap = new HashMap<String, BuildingBlock>();
+		HashMap<Long, BuildingBlock> configHashMap = new HashMap<Long, BuildingBlock>();
+		
+	}
+	
+	
+	
+	public boolean isUpdated( ) {
+		return false;
+		}
+	public boolean addInitBuildingBlock(String id, BuildingBlock block ) {
+		return false;
+		}
+	public boolean addConfigBuildingBlock(int id, BuildingBlock block ) {
+		return false;
+	}
+	public boolean removeConfigBuildingBlock(String id ) {
+		return false;
+		}
+	
+	//public BuildingBlock getInitBuildingBlock(String id ) {}
+	//public BuildingBlock getConfigBuildingBlock(int id ) {}
+	public HashMap<String, BuildingBlock> getFullInitBlock() {
+		return initHashMap;
+	}
+	
+	
+	
+	
+	public HashMap<String, BuildingBlock> getFullConfigBlock() {
+		return configHashMap;
+	}
+	public boolean addConfigConnection(String bbId1,  int chId1, int chId2, String bbId2) {
+		return false;
+		}
+	public boolean removeConfigConnection(String bbId1,  int chId1, int chId2, String bbId2) {
+		return false;
+		}
+	public boolean getConnection( int chId1, int chId2) {
+		return false;
+		}
+	public boolean setUpdate( boolean bool) {
+		return false;
+		}
+	public boolean checkUpdate() {
+		return false;
+		}
+	
+	
+	
+	
+	
+	
 	
 }
