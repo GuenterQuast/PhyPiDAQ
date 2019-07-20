13eea95b877d62a97765a14aa95a145e31402222
Guenter Quast <guenter.quast@online.de>
2019-07-20 17:50:54 +0200

    added python3-yaml

--- installlibs.sh
+++ installlibs.sh
@@ -5,11 +5,11 @@
 # -------------------------------------------------
 
 
+sudo apt-get install python3-yaml
 sudo apt-get install python3-scipy
 sudo apt-get install python3-matplotlib
 sudo apt-get install python3-pyqt5
 sudo apt-get install libatlas-base-dev # needed to build nupmy
-
 sudo pip3 install installlibs/whl/*.whl
 sudo dpkg -i installlibs/picoscopelibs/*.deb
 
