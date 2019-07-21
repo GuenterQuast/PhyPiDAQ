8c7f955458968821d31ea37db9a032e795600753
Guenter Quast <guenter.quast@online.de>
2019-07-21 16:41:08 +0200

    installation of packages with c-libraries from source

--- installlibs.sh
+++ installlibs.sh
@@ -2,16 +2,18 @@
 #
 # script to install libraries PhyPiDAQ depends on
 #
-# -------------------------------------------------
-
+# -----------------------------------------------
 
 sudo apt-get install python3-yaml
 sudo apt-get install python3-scipy
 sudo apt-get install python3-matplotlib
 sudo apt-get install python3-pyqt5
 sudo apt-get install libatlas-base-dev # needed to build nupmy
-sudo pip3 install installlibs/whl/*.whl
-sudo dpkg -i installlibs/picoscopelibs/*.deb
 
+sudo pip3 install installlibs/whl/*.whl # python wheels
+
+sudo pip3 install installlibs/tgz/*.tar.gz # python packages 
+
+sudo dpkg -i installlibs/picoscopelibs/*.deb # picoscope 
 sudo usermod -a -G tty pi # grant acces to USB for user pi
 
