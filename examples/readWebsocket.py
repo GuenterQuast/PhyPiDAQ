3fb2ec41c35e8b579a8f767258f07763a54b2a85
Guenter Quast <guenter.quast@online.de>
2022-01-30 15:25:35 +0100

    added script to send data via and read from web socket

---
+++ examples/readWebsocket.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Read data in CSV format from websocket
+"""
+
+import numpy, time
+import sys, asyncio
+import websockets
+
+# read url from command line
+if len(sys.argv)>=2:
+  uri = sys.argv[1]
+else:
+  # host url and port
+  uri = "ws://localhost:8314"
+print("*==* ", sys.argv[0], " Lese Daten von url ", uri)
+
+async def read_ws():
+  """asyncronous read from websocket 
+  """    
+  async with websockets.connect(uri) as websocket:
+    # test connection  
+    await websocket.send("req_connect")
+    answ = await websocket.recv()
+    if answ == "ack_connect":
+      print("** connected to websocket ", uri)
+    
+  # get data
+    await websocket.send("getData")
+    while True:
+      inp = await websocket.recv()
+      if inp == '\n': # empty record, end
+        print("empty input - closing")
+        sys.exit(0)
+      else:
+        print('read: %s '%inp, end='')
+
+# run web client          
+asyncio.get_event_loop().run_until_complete(read_ws())
---
+++ examples/sendPipe2ws.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" script sendPipe2ws.py
+    start script as background process: 
+    sendPipe2ws.py [name of pipe] & 
+
+    Read data from a named linux pipe 
+    filled by run_phypi.py with option 
+    DAQfifo: <name of pipe>
+    and send result to websocket on localhost:8314
+
+    read data with script
+    readWebsocket.py ws://localhost:8314
+"""
+
+import sys, os, errno
+import asyncio, websockets
+
+if len(sys.argv)>=2:
+  FiFo = sys.argv[1]
+else:
+  FiFo = "PhyPiDAQ.fifo"
+print('*==* ', sys.argv[0], ' Lese Daten aus Pipe',  FiFo)
+  
+# first, open FiFo to connect to runPhyPiDAQ
+#    ignore error if it already exists
+try:
+  os.mkfifo(FiFo)
+except OSError as e:
+  if e.errno != errno.EEXIST:
+    raise
+
+# set up a websocket for acces via ws://localhost:8314  
+host = 'localhost'
+port = 8314
+dbg = False  
+async def data_provider(websocket, path):
+  async for message in websocket:
+    if dbg: print("server got: ", message)
+
+    if message == "req_connect":
+    # confirm connection 
+      await websocket.send("ack_connect")
+    else:
+    #  get and send data
+      with open(FiFo) as f:
+        for inp in f:
+          await websocket.send( inp )
+          if inp == '\n':
+            print("empty input - end")
+            break
+      print("end of file reached - closing")
+      sys.exit(0)
+
+# start web service 
+print('** server running under uri ws://'+host+':',port)
+start_server = websockets.serve(data_provider, host, port)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
+
+
+
+
+  
+      
+      
